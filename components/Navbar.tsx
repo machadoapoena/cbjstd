d1f48d26a0ce00d34402c8340d727d8be9de9228
Apoena <machadoapoena@gmail.com>
2026-02-12 14:40:41 -0300

    feat(navbar): Update tournament name text
    Changes the "JUVENIL" text in the navbar to "DE CATEGORIAS" to more accurately reflect the tournament's scope.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -32,7 +32,7 @@ const Navbar: React.FC = () => {
         <a href="#home" className="flex items-center gap-3">
           <Logo size={40} className="text-neutral-900" />
           <span className="font-oswald text-xl font-bold italic tracking-tighter uppercase text-neutral-900 leading-none">
-            {TOURNAMENT_NAME.split(' ')[1]} <span className="text-lime-600">JUVENIL</span>
+            {TOURNAMENT_NAME.split(' ')[1]} <span className="text-lime-600">DE CATEGORIAS</span>
           </span>
         </a>
 
